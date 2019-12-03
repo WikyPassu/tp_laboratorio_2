ccb63b8bc756e8d05ce2cc5d34db1d3aa1527ca6
WikyPassu <alan_passucci@hotmail.com>
2019-12-02 22:17:29 -0300

    Recuperatorio TP2

---
+++ RecuperatoriosTP/TP2/Entidades/Changuito.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2018
+{
+    /// <summary>
+    /// No podrá tener clases heredadas.
+    /// </summary>
+    public sealed class Changuito
+    {
+        List<Producto> productos;
+        private int espacioDisponible;
+
+        public enum ETipo
+        {
+            Dulce,
+            Leche,
+            Snacks,
+            Todos
+        }
+
+        #region "Constructores"
+        /// <summary>
+        /// Constructor del Changuito que inicializa la lista de productos
+        /// </summary>
+        private Changuito()
+        {
+            this.productos = new List<Producto>();
+        }
+
+        /// <summary>
+        /// Constructor del Changuito que inicializa el espacio disponible
+        /// </summary>
+        /// <param name="espacioDisponible"></param>
+        public Changuito(int espacioDisponible)
+            : this()
+        {
+            this.espacioDisponible = espacioDisponible;
+        }
+        #endregion
+
+        #region "Sobrecargas"
+        /// <summary>
+        /// Muestro el Changuito y TODOS los Productos
+        /// </summary>
+        /// <returns>Retorna todos los Productos en el Changuito</returns>
+        public override string ToString()
+        {
+            return Changuito.Mostrar(this, ETipo.Todos);
+        }
+        #endregion
+
+        #region "Métodos"
+
+        /// <summary>
+        /// Expone los datos del elemento y su lista (incluidas sus herencias)
+        /// SOLO del tipo requerido
+        /// </summary>
+        /// <param name="c">Elemento a exponer</param>
+        /// <param name="ETipo">Tipos de ítems de la lista a mostrar</param>
+        /// <returns>Retorna todos los Productos en el Changuito en formato string</returns>
+        public static string Mostrar(Changuito c, ETipo tipo)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendFormat("Tenemos {0} lugares ocupados de un total de {1} disponibles\n", c.productos.Count, c.espacioDisponible);
+            sb.AppendLine("");
+            foreach (Producto v in c.productos)
+            {
+                switch (tipo)
+                {
+                    case ETipo.Snacks:
+                        if(v is Snacks)
+                        {
+                            sb.AppendLine(v.Mostrar());
+                        }
+                        break;
+                    case ETipo.Dulce:
+                        if(v is Dulce)
+                        {
+                            sb.AppendLine(v.Mostrar());
+                        }
+                        break;
+                    case ETipo.Leche:
+                        if(v is Leche)
+                        {
+                            sb.AppendLine(v.Mostrar());
+                        }   
+                        break;
+                    default:
+                        sb.AppendLine(v.Mostrar());
+                        break;
+                }
+            }
+
+            return sb.ToString();
+        }
+        #endregion
+
+        #region "Operadores"
+        /// <summary>
+        /// Agregará un elemento a la lista
+        /// </summary>
+        /// <param name="c">Objeto donde se agregará el elemento</param>
+        /// <param name="p">Objeto a agregar</param>
+        /// <returns>Retorna el objeto Changuito</returns>
+        public static Changuito operator +(Changuito c, Producto p)
+        {
+            bool existe = false;
+
+            if (!Object.Equals(c, null) && !Object.Equals(p, null))
+            {
+                if (c.espacioDisponible > c.productos.Count)
+                {
+                    foreach (Producto v in c.productos)
+                    {
+                        if (v == p)
+                        {
+                            existe = true;
+                            break;
+                        }
+                    }
+                    if (!existe)
+                    {
+                        c.productos.Add(p);
+                    }
+                }
+            }
+            
+            return c;
+        }
+        /// <summary>
+        /// Quitará un elemento de la lista
+        /// </summary>
+        /// <param name="c">Objeto donde se quitará el elemento</param>
+        /// <param name="p">Objeto a quitar</param>
+        /// <returns>Retorna el objeto Changuito</returns>
+        public static Changuito operator -(Changuito c, Producto p)
+        {
+            if (!Object.Equals(c, null) && !Object.Equals(p, null))
+            {
+                if (c.productos.Count > 0)
+                {
+                    foreach (Producto v in c.productos)
+                    {
+                        if (v == p)
+                        {
+                            c.productos.Remove(v);
+                            break;
+                        }
+                    }
+                }
+            }
+            
+            return c;
+        }
+        #endregion
+    }
+}
---
+++ RecuperatoriosTP/TP2/Entidades/Leche.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+
+namespace Entidades_2018
+{
+    public class Leche : Producto
+    {
+        /// <summary>
+        /// TIPO de Leche
+        /// </summary>
+        public enum ETipo
+        {
+            Entera,
+            Descremada
+        }
+
+        ETipo tipo;
+
+        /// <summary>
+        /// Instancia los datos de la Leche. Por defecto, TIPO será ENTERA
+        /// </summary>
+        /// <param name="marca">Marca de la Leche</param>
+        /// <param name="codigo">Codigo de barras de la Leche</param>
+        /// <param name="color">Color del empaque de la Leche</param>
+        public Leche(EMarca marca, string codigo, ConsoleColor color)
+            : base(codigo, marca, color)
+        {
+            this.tipo = ETipo.Entera;
+        }
+
+        /// <summary>
+        /// Instancia los datos de la Leche.
+        /// </summary>
+        /// <param name="marca">Marca de la Leche</param>
+        /// <param name="codigo">Codigo de barras de la Leche</param>
+        /// <param name="color">Color del empaque de la Leche</param>
+        /// <param name="tipo">Tipo de Leche</param>
+        public Leche(EMarca marca, string codigo, ConsoleColor color, ETipo tipo)
+            : this(marca, codigo, color)
+        {
+        }
+     
+        /// <summary>
+        /// Devuelve la cantidad de calorías de la Leche (20)
+        /// </summary>
+        protected override short CantidadCalorias
+        {
+            get
+            {
+                return 20;
+            }
+        }
+
+        /// <summary>
+        /// Muestra todos los datos de la Leche
+        /// </summary>
+        /// <returns>Retorna todos los datos de la Leche en formato string</returns>
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("LECHE");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine("CALORIAS : " + this.CantidadCalorias);
+            sb.AppendLine("TIPO : " + this.tipo);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
